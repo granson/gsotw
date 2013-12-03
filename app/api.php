6be3c426a69de754604ba86906cb3f8166158fa3
granson <grant.ranson@gmail.com>
2013-12-02 23:54:37 -0700

    Remaining CRUD Functions

--- app/api.php
+++ app/api.php
@@ -4,24 +4,47 @@
 getApi()->get('/api/page/read/(\w+)', array('Api', 'pageRead'), EpiApi::external);
 getApi()->get('/api/page/write', array('Api', 'pageWrite'), EpiApi::external);
 
-class Api {
-	function pageRead( $pageId ) {
 
+// CRUD: Create, Read, Update, Delete
+
+class Api {
+
+	// Common Array() Code
+	function createArray()
+	{
+	 $page=array(
+	 "title" => $_GET['title'],
+	 "visible" => $_GET['visible'],
+	 "content" => $_GET['content'],
+	 "route" => $_GET['route']
+	 );
+	 $filename = str_replace("'", "", $page['route']);
+	}
+
+	// CREATE
+	function pageCreate()
+	{
+	 call_user_func('createArray');
+	 fopen("content/" . $filename . ".json", 'w');
+	}
+
+	// READ
+	function pageRead( $pageId ) {
 	  	return file_get_contents ( "content/" .$pageId . ".json");
 	}
 
-	function pageWrite() {
-		$page=array(
-		"title" => $_GET['title'],
-		"visible" => $_GET['visible'],
-		"content" => $_GET['content'],
-		"route" => $_GET['route']
-		);
+	// UPDATE
+	function pageUpdate() {
+	call_user_func('createArray');
+	file_put_contents ("content/" . $filename . ".json", json_encode($page));
 
-		$filename = str_replace("'", "", $page['route']);
-
-		file_put_contents ("content/" . $filename . ".json", json_encode($page));
-
-		return 200;
+	return 200;
 	}
+
+	// DELETE
+	function pageDelete($pageId)
+	{
+	 unlink ("content/" . $pageId . ".json");
+	}
+
 }
